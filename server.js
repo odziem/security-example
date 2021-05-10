82995e4bc7a7f30b5ce5635263bec113021b4121
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-05-10 01:40:26 -0400

    With secret endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 const app = express();
 
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
