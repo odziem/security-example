297c95aa8d168885a23a38bdf284f5369ef807fd
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-05-14 04:38:29 -0400

    Add helmet

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
   return res.send('Your personal secret value is 42!');
